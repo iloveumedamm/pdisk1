a8cb364079f5014b9131c2c28862f4a13b2491d3
iloveumedamm <140915401+iloveumedamm@users.noreply.github.com>
2024-03-07 07:14:32 +0530

    Update database.py

--- FileStream/utils/database.py
+++ FileStream/utils/database.py
@@ -153,7 +153,7 @@ async def get_user(user_id):
 
     user_id = int(user_id)
 
-    user = await col.find_one({"user_id": user_id})
+    user = await self.col.find_one({"user_id": user_id})
 
     if not user:
         res = {
@@ -162,8 +162,8 @@ async def get_user(user_id):
             "base_site": None,
         }
 
-        await col.insert_one(res)
-        user = await col.find_one({"user_id": user_id})
+        await self.col.insert_one(res)
+        user = await self.col.find_one({"user_id": user_id})
 
     return user
 
@@ -175,14 +175,14 @@ async def update_user_info(user_id, value:dict):
     user_id = int(user_id)
     myquery = {"user_id": user_id}
     newvalues = { "$set": value }
-    await col.update_one(myquery, newvalues)
+    await self.col.update_one(myquery, newvalues)
 
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
 # Ask Doubt on telegram @KingVJ01
 
 async def total_users_count():
-    count = await col.count_documents({})
+    count = await self.file.count_documents({})
     return count
 
 # Don't Remove Credit Tg - @VJ_Botz
@@ -190,7 +190,7 @@ async def total_users_count():
 # Ask Doubt on telegram @KingVJ01
 
 async def get_all_users():
-    all_users = col.find({})
+    all_users = self.col.find({})
     return all_users
 
 # Don't Remove Credit Tg - @VJ_Botz
@@ -198,7 +198,7 @@ async def get_all_users():
 # Ask Doubt on telegram @KingVJ01
 
 async def delete_user(user_id):
-    await col.delete_one({'user_id': int(user_id)})
+    await self.col.delete_one({'user_id': int(user_id)})
 
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
