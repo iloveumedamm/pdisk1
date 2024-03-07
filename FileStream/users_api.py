1eb9b4df4a1e467d0f795f7777226affe8329c48
iloveumedamm <140915401+iloveumedamm@users.noreply.github.com>
2024-03-07 07:06:22 +0530

    Update users_api.py

--- FileStream/users_api.py
+++ FileStream/users_api.py
@@ -3,7 +3,7 @@
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
 # Ask Doubt on telegram @KingVJ01
-from FileStream.config import Telegram
+from FileStream.config import Telegram, Telegram.SESSION_NAME, Telegram.DATABASE_URL
 from FileStream.utils.database import Database
 import requests
 import json
