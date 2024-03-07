4c86c1e5a3fc0db90918a1722952e01f220176b1
iloveumedamm <140915401+iloveumedamm@users.noreply.github.com>
2024-03-07 06:58:51 +0530

    Update config.py

--- FileStream/config.py
+++ FileStream/config.py
@@ -10,6 +10,7 @@ class Telegram:
     OWNER_ID = int(env.get('OWNER_ID', '5791145987'))
     WORKERS = int(env.get("WORKERS", "6"))  # 6 workers = 6 commands at once
     DATABASE_URL = str(env.get('DATABASE_URL', "mongodb+srv://lajihi2115:lgAEiuZHs917nZgy@cluster0.lx88eg8.mongodb.net/?retryWrites=true&w=majority"))
+    DB_NAME = str(env.get('SESSION_NAME', 'FileStream'))
     UPDATES_CHANNEL = str(env.get('UPDATES_CHANNEL', "Telegram"))
     SESSION_NAME = str(env.get('SESSION_NAME', 'FileStream'))
     FORCE_SUB_ID = env.get('FORCE_SUB_ID', None)
