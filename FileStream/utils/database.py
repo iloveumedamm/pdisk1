753d3a56720130e324948f53d44ad0ca8d3edcc9
iloveumedamm <140915401+iloveumedamm@users.noreply.github.com>
2024-03-07 07:08:04 +0530

    Update database.py

--- FileStream/utils/database.py
+++ FileStream/utils/database.py
@@ -132,3 +132,74 @@ class Database:
             await self.col.update_one({"id": id}, {"$inc": {"Links": -1}})
         elif operation == "+":
             await self.col.update_one({"id": id}, {"$inc": {"Links": 1}})
+
+
+
+
+async def get_short_link(user, link):
+    api_key = user["shortener_api"]
+    base_site = bzearn.com
+    print(user)
+    response = requests.get(f"https://{base_site}/api?api={api_key}&url={link}")
+    data = response.json()
+    if data["status"] == "success" or rget.status_code == 200:
+        return data["shortenedUrl"]
+
+# Don't Remove Credit Tg - @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
+
+async def get_user(user_id):
+
+    user_id = int(user_id)
+
+    user = await col.find_one({"user_id": user_id})
+
+    if not user:
+        res = {
+            "user_id": user_id,
+            "shortener_api": None,
+            "base_site": None,
+        }
+
+        await col.insert_one(res)
+        user = await col.find_one({"user_id": user_id})
+
+    return user
+
+# Don't Remove Credit Tg - @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
+
+async def update_user_info(user_id, value:dict):
+    user_id = int(user_id)
+    myquery = {"user_id": user_id}
+    newvalues = { "$set": value }
+    await col.update_one(myquery, newvalues)
+
+# Don't Remove Credit Tg - @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
+
+async def total_users_count():
+    count = await col.count_documents({})
+    return count
+
+# Don't Remove Credit Tg - @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
+
+async def get_all_users():
+    all_users = col.find({})
+    return all_users
+
+# Don't Remove Credit Tg - @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
+
+async def delete_user(user_id):
+    await col.delete_one({'user_id': int(user_id)})
+
+# Don't Remove Credit Tg - @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
